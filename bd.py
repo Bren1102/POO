9da616dce37872615bed9835dfb9008f62529d00
Bren1102 <bmorrone@hotmail.com>
2024-10-14 18:42:45 -0300

    Clase SchemaError

--- bd.py
+++ bd.py
@@ -56,11 +56,17 @@ class BaseDeDatosDocumental:
 schema = 'Nombre,Apellido,Edad,Mail'
 row = 'Brenda,Morrone,36,bmorrone@hotmail.com'
 
+
+class SchemaError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
 class Str2Dic():
     def __init__(self, schema, separator=','):
         self.schema = schema.split(separator)
         self.separator = separator
-    def convert(self, row)
+    def convert(self, row):
         tmp = row.split(self.separator)
         if len(tmp) == len(self.schema):
             i = 0
@@ -69,6 +75,8 @@ class Str2Dic():
                 d[self.schema[i]] = tmp[i]
                 i += 1
             return d
+        else:
+            raise SchemaError("Los campos de la fila no concuerdan con el schema")
 
             
         
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from bd import Coleccion
 from bd import Documento
+from bd import Str2Dic
 
 def import_collection(nombre_archivo):
     Coleccion = Coleccion()
 
+    with open(Documento, 'r') as f:
+        schema = f.readline().rstrip()
+        diccionario_schema = Str2Dic(schema)
+
+
+    
+class SchemaError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+   
    
 
     
