741edcf25c1be581069d6eb9f53c4d82b319705d
Bren1102 <bmorrone@hotmail.com>
2024-11-17 13:41:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+import csv
+
 from bd import Coleccion
 from bd import Documento
 from bd import Str2Dic
 
-def import_collection(nombre_archivo):
-    Coleccion = Coleccion()
+
+def import_collection(self, nombre_archivo):
+    coleccion = Coleccion()
 
     with open(Documento, 'r') as f:
         schema = f.readline().rstrip()
         diccionario_schema = Str2Dic(schema)
+        linea = coleccion.readline().split()
+        while linea != "":
+           d = diccionario_schema.convert(linea)
+           doc = Documento(self.id_documento, d)
+           self.add_document(doc) 
+           self.id_documento = self.id_documento + 1
+        #leer de vuelta al final
+           linea = coleccion.readline
 
+c = Coleccion()
+c.import_collection('datos_personales.csv')
 
-    
-class SchemaError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(message)
    
    
 
